145258ffa5abcbb262e1c232da521cd9cba69b92
Aaron Chafetz <achafetz@usaid.gov>
2023-12-01 10:06:39 -0500

    fix replace baseurl since it does not exist

--- R/extract_datim.R
+++ R/extract_datim.R
@@ -376,8 +376,10 @@ datim_execute_query <- function(url,
       usethis::ui_info(paste0("Query url: ", query_url))
       usethis::ui_info(paste0("Query status: ", .res$status_code))
 
+      query_url_base <- stringr::str_extract(query_url, '(?<=www\\.).*(?=/api)')
+
       if(.res$status_code == 401) {
-        usethis::ui_stop("No access to the url. Check that your credentials, used or stored, are current and that you have access to {baseurl}")
+        usethis::ui_stop("No access to the url. Check that your credentials, used or stored, are current and that you have access to {query_url_base}")
       }
 
       # Reject non 200 (OK) responses
