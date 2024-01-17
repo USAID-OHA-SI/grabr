e3c3fd4c65d5df57aa5dff825c8d110cf664f55c
Baboyma Kagniniwa <baboyma@gmail.com>
2024-01-17 09:17:10 -0500

    TEST - Adding testing module with testthat

--- tests/testthat/test-datim-lookups.R
+++ tests/testthat/test-datim-lookups.R
@@ -2,18 +2,4 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-# Test get_ouuid
 
-test_that("Looking up OU UID works", {
-  expect_equal(get_ouuid(operatingunit = "Nigeria"), "PqlFzhuPcF1")
-})
-
-# Test get_ouorglevel
-
-test_that("Looking up OU Org Levels", {
-  expect_equal(get_ouorglabel(operatingunit = "Nigeria", org_level = 3), "country")
-})
-
-test_that("Looking up OU Org Label", {
-  expect_equal(get_ouorglevel(operatingunit = "Cote d'Ivoire", org_type = "country"), 3)
-})
