55364374402d21cb517010e6b8501b22d6f71203
Aaron Chafetz <achafetz@usaid.gov>
2024-04-02 20:27:24 +0000

    feat: add pdap to lazy_secrets

--- R/utilities.R
+++ R/utilities.R
@@ -71,19 +71,18 @@ var_exists <- function(df, var) {
 #' stored via `glamr` or prompts user to provide credentials through interactive
 #' prompt.
 #'
-#' @param service account, either datim", "pano", or "s3"
+#' @param service account, either datim", "pano", "pdap", or "s3"
 #' @param username account username or s3 access key
 #' @param password account password or s3 secret key
 #'
 #' @return returns a list of 2 - username/access and password/secret
 #' @export
-#' @keywords internal
 #' @examples
 #' \dontrun{
 #'   accnt <- lazy_secrets("datim", username = username, password = password)
 #'   datim_dimensions(accnt$username, accnt$password)
 #' }
-lazy_secrets <- function(service = c("datim", "pano", "s3"),
+lazy_secrets <- function(service = c("datim", "pano", "pdap", "s3"),
                          username, password){
 
   check_internet()
@@ -92,9 +91,15 @@ lazy_secrets <- function(service = c("datim", "pano", "s3"),
   service <- match.arg(service)
 
   #use stored secrets created under glamr if available (and nothing provided)
-  if(service != "s3" && missing(username) && glamr::is_stored(service)){
+  if(service %in% c("datim", "pano") && missing(username) && glamr::is_stored(service)){
     username <- keyring::key_list(service)[1, 2]
     password <- keyring::key_get(service, username)
+  }
+
+  #if pdap, use stored secrets created under glamr if available (and nothing provided)
+  if(service == "pdap" && missing(username) && glamr::is_stored(service)){
+    username <- glamr::pdap_access()
+    password <- glamr::pdap_secret()
   }
 
   #if s3, use stored secrets created under glamr if available (and nothing provided)
@@ -106,17 +111,17 @@ lazy_secrets <- function(service = c("datim", "pano", "s3"),
   #if no username, prompt (UI) for username
   if(missing(username) && !glamr::is_stored(service))
     username <- getPass::getPass(
-      glue::glue("Provide {service} {ifelse(service == 's3', 'access key','username')}")
+      glue::glue("Provide {service} {ifelse(service %in% c('pdap', 's3'), 'access key','username')}")
       )
 
   #if no username, prompt (UI) for username
   if(missing(password) && !glamr::is_stored(service))
     password <- getPass::getPass(
-      glue::glue("Provide {service} {ifelse(service == 's3', 'secret key','password')}")
+      glue::glue("Provide {service} {ifelse(service %in% c('pdap', 's3'), 'secret key','password')}")
     )
 
   #if s3, change names from user/pass to access/secret key
-  if(service != "s3"){
+  if(service %in% c("datim", "pano")){
     accnt_info <- list(username = username,
                        password = password)
   } else {
